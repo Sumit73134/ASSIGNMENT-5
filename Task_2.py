a900329f6548dc637eeca9fb01110afb7a164376
Sumit Kumar <sumitshrivastav416@gmail.com>
2025-09-15 16:17:35 +0530

    Demonstrate List Slicing

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+num = [1,2,3,4,5,6,7,8,9,10]
+
+first_five = num[:5]
+
+Reverse = first_five[::-1]
+
+print("Original list: ",num)
+print("Extracted first five elements: ",first_five)
+print("Reversed extracted elements: ",Reverse)
